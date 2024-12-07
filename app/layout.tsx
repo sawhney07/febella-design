170ed4eb729fbd95a792605e0c4758104592c9a6
Armaan <armaan070903@gmail.com>
2024-12-06 20:13:30 -0800

    small icon metadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,20 @@ import Navigation from './navigation/page'
 export const metadata: Metadata = {
   title: 'Febella Design',
   description: 'Fashion consulting and personal styling services',
+  openGraph: {
+    title: 'Febella Design',
+    description: 'Fashion consulting and personal styling services',
+    url: 'https://febelladesign.com',
+    siteName: 'Febella Design',
+    images: [
+      {
+        url: '/favicon.ico', // Specify the path to your image or favicon for social media previews
+        width: 800,
+        height: 800,
+        alt: 'Febella Design',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
